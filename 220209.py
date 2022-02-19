fd7efc11cb29db578f1b5486c90c8875d0f994c1
SiYeon-Kwon <83439685+SiYeon-Kwon@users.noreply.github.com>
2022-02-19 23:54:27 +0900

    Update 220209.py

--- 220209.py
+++ 220209.py
@@ -43,9 +43,9 @@ validation_dataset = datasets.ImageFolder(valid_dir, transform = validation_tran
 test_dataset = datasets.ImageFolder(test_dir, transform = test_transform)
 
 # TODO: Using the image datasets and the trainforms, define the dataloaders
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = 64, shuffle = True)
-validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size = 32)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 32)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size = 32, shuffle = True)
+validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size = 16)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = 16)
 #print(type(train_loader))
 # Label mapping
 
@@ -54,7 +54,7 @@ with open('C:/Windows/System32/kwontest/experiment/train/mergefile.json', 'r') a
     json_file = json.load(f)
     
 # Bulid and train the classifier
-model = models.efficientnet_b7(pretrained=False)
+model = models.efficientnet_b7(pretrained=True)
 
 # Freeze pretrained model parameters to avoid backpropagation
 for parameter in model.parameters():
@@ -65,7 +65,7 @@ from collections import OrderedDict
 # Build custom classifier
 classifier = nn.Sequential(OrderedDict([('fc1', nn.Linear(2560, 32)),
                                         ('relu', nn.ReLU()),
-                                        ('drop', nn.Dropout(p=0.3)),
+                                        ('drop', nn.Dropout(p=0.5)),
                                         ('fc2', nn.Linear(32, 2560)),
                                         ('output', nn.LogSoftmax(dim=1))]))
 model.classifier = classifier
@@ -95,10 +95,57 @@ def validation(model, validation_loader, criterion):
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.classifier.parameters(), lr=0.001)
 
+class EarlyStopping:
+    """주어진 patience 이후로 validation loss가 개선되지 않으면 학습을 조기 중지"""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt'):
+        """
+        Args:
+            patience (int): validation loss가 개선된 후 기다리는 기간
+                            Default: 7
+            verbose (bool): True일 경우 각 validation loss의 개선 사항 메세지 출력
+                            Default: False
+            delta (float): 개선되었다고 인정되는 monitered quantity의 최소 변화
+                            Default: 0
+            path (str): checkpoint저장 경로
+                            Default: 'checkpoint.pt'
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Save model if validation loss decrease'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+        
 
 # Train the classifier
 
-from workspace_utils import active_session
+#from workspace_utils import active_session
 
 def train_classifier():
     
@@ -107,8 +154,11 @@ def train_classifier():
         epochs = 100
         steps = 1000
         print_every = 10
+        patience = 20
         
         model.to('cuda')
+        
+        early_stopping = EarlyStopping(patience = patience, verbose = True)
         
         for e in range(epochs):
             
@@ -144,55 +194,30 @@ def train_classifier():
                           "Validation Loss: {:.3f}.. ".format(validation_loss/len(validation_loader)),
                           "Validation Accuracy: {:.3f}".format(accuracy/len(validation_loader)))
                     
+                    early_stopping(validation_loss, model)
+                    
+                    if early_stopping.early_stop:
+                     print("Early stopping")
+                     break
+                    
                     running_loss = 0
                     model.train()
                     
 train_classifier()
 
 
-
-#Test Network
-
-def test_accuracy(model, test_loader):
-    
-    # Do  validation on the test set
-    model.eval()
-    model.to('cuda')
-    
-    with torch.no_grad():
-        
-        accuracy = 0
-        
-        for images, labels in iter(test_loader):
-            
-            images, labels = images.to('cuda'), labels.to('cuda')
-            
-            output = model.forward(images)
-            
-            probabilities = torch.exp(output)
-            
-            equality = (labels.data == probabilities.max(dim=1)[1])
-            
-            accuracy += equality.type(torch.FloatTensor).mean()
-            
-        print("Test Accuracy: {}".format(accuracy/len(test_loader)))
-        
-test_accuracy(model, test_loader)
-        
-
-
 # Save the checkpoint
 
-'''def save_checkpoint(model):
+def save_checkpoint(model):
     
-    model.class_to_idx = train_dataset.class_do_idx
+    model.class_to_idx = train_dataset.class_to_idx
     
-    checkpoint = {'arch': "vgg16",
+    checkpoint = {'arch': "efficientnet",
                    'class_to_idx': model.class_to_idx,
                    'model_state_dict': model.state_dict()
                    }
     
-    torch.save(checkpoint, 'checkpoint.pth')
+    torch.save(checkpoint, 'C:/Windows/System32/kwontest/checkpoint.pth')
     
 save_checkpoint(model)
 
@@ -207,7 +232,7 @@ def load_checkpoint(filepath):
     
     checkpoint = torch.load(filepath)
     
-    if checkpoint['arch'] == 'vgg16':
+    if checkpoint['arch'] == 'efficientnet':
         
         model = models.vgg16(pretrained=True)
         
@@ -230,21 +255,106 @@ def load_checkpoint(filepath):
     model.load_state_dict(checkpoint['model_state_dict'])
     
     return model
-'''
 
-'''
+
 #Image Preprocessing
-from PIL import Images
+from PIL import Image
 
-def preprocess_images(image_path):
+def process_image(image_path):
     
-    pil_images = Image.open(image_path)
+    # Process a PIL image for use in a PyTorch model
     
-    #Resize
+    pil_image = Image.open(image_path)
+    
+    # Resize
     if pil_image.size[0] > pil_image.size[1]:
         pil_image.thumbnail((5000, 256))
     else:
         pil_image.thumbnail((256, 5000))
         
-    #Crop
-'''
+    # Crop 
+    left_margin = (pil_image.width-224)/2
+    bottom_margin = (pil_image.height-224)/2
+    right_margin = left_margin + 224
+    top_margin = bottom_margin + 224
+    
+    pil_image = pil_image.crop((left_margin, bottom_margin, right_margin, top_margin))
+    
+    # Normalize
+    np_image = np.array(pil_image)/255
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    np_image = (np_image - mean) / std
+    
+    # PyTorch expects the color channel to be the first dimension but it's the third dimension in the PIL image and Numpy array
+    # Color channel needs to be first; retain the order of the other two dimensions.
+    np_image = np_image.transpose((2, 0, 1))
+    
+    return np_image
+
+def imshow(image, ax=None, title=None):
+    if ax is None:
+        fig, ax = plt.subplots()
+    
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.transpose((1, 2, 0))
+    
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+    
+    if title is not None:
+        ax.set_title(title)
+    
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+    
+    ax.imshow(image)
+    
+    return ax
+
+image = process_image('C:/Windows/System32/kwontest/experiment/test/retro/91.jpg')
+imshow(image)
+
+# Implement the code to predict the class from an image file
+
+def predict(image_path, model, topk=5):
+    '''Predict the class (or classes) of an image using a trained deep learning model.
+    '''
+
+    image = process_image(image_path)
+    
+    # Convert image to PyTorch tensor first
+    image = torch.from_numpy(image).type(torch.cuda.FloatTensor)
+    #print(image.shape)
+    #print(type(image))
+    
+    # Returns a new tensor with a dimension of size one inserted at the specified position.
+    image = image.unsqueeze(0)
+    
+    output = model.forward(image)
+    
+    probabilities = torch.exp(output)
+    
+    # Probabilities and the indices of those probabilities corresponding to the classes
+    top_probabilities, top_indices = probabilities.topk(topk)
+    
+    # Convert to lists
+    top_probabilities = top_probabilities.detach().type(torch.FloatTensor).numpy().tolist()[0] 
+    top_indices = top_indices.detach().type(torch.FloatTensor).numpy().tolist()[0] 
+    
+    # Convert topk_indices to the actual class labels using class_to_idx
+    # Invert the dictionary so you get a mapping from index to class.
+    
+    idx_to_class = {value: key for key, value in model.class_to_idx.items()}
+    #print(idx_to_class)
+    
+    top_classes = [idx_to_class[index] for index in top_indices]
+    
+    return top_probabilities, top_classes
+    
+probs, classes = predict('C:/Windows/System32/kwontest/experiment/test/retro/91.jpg', model)   
+print(probs)
+print(classes)
